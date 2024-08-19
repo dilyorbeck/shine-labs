30a6a6cf6da90a566dc3f0d0cfab4b76a9ef6191
Dilyorbek <justsharofiddinov@gmail.com>
2024-08-19 18:17:43 +0500

    hero and shopping section is finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      'brown-50': '#ffe1cc',
+      'brown-150': '#f8ad79',
+      'brown-950': '#462b13',
+      yellow: '#ffb700',
+      white: '#fff',
+    },
+    extend: {
+      padding: {
+        138: '138px',
+        102: '102px',
+      },
+      margin: {
+        18: '18px'
+      },
+      backgroundImage: {
+        'brown-gradient': 'linear-gradient(to bottom, #422410 100%, #2D1A0F 64%)',
+      },
+      fontSize: {
+        42: '42px',
+        50: '50px',
+        21: '21px',
+      },
+      lineHeight: {
+        56: '56px',
+      },
+      borderRadius: {
+        10: '10px',
+      },
+    },
   },
   plugins: [],
 }
