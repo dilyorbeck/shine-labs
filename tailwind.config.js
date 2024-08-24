51b7a5283224a213139eb65429810022e64817ea
Dilyorbek <justsharofiddinov@gmail.com>
2024-08-24 17:09:35 +0500

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,14 @@ module.exports = {
       'gray-50': '#f7f7f7',
       'gray-150': '#e8e8e8',
       'gray-250': '#f6f6f6',
-      'gray-350':  '#1c1c1c', 
+      'gray-350': '#1c1c1c',
+      'gray-450': '#A6A6A6',
       yellow: '#ffb700',
       white: '#fff',
       black: '#000',
       lime: '#60d6b1',
       grayishBrown: '#f9eee6;',
+      red: '#ff0000',
     },
     fontFamily: {
       redhat: ['"Red Hat Display"'],
@@ -23,7 +25,19 @@ module.exports = {
     },
     extend: {
       height: {
-        446: '446px'
+        446: '446px',
+        72: '72px',
+        30: '30px',
+        106: '106px',
+        507: '507px',
+        367: '367px',
+
+      },
+      width: {
+        864: '864px',
+        840: '840px',
+        360: '360px',
+        30: '30px'
       },
       padding: {
         138: '138px',
@@ -34,26 +48,41 @@ module.exports = {
       },
       margin: {
         18: '18px',
+        30: '30px'
       },
       backgroundImage: {
-        'brown-gradient': 'linear-gradient(to bottom, #422410 100%, #2D1A0F 64%)',
+        'brown-gradient': 'linear-gradient(to bottom, #422410 100%, #2D1A0F 64%);'
       },
       fontSize: {
         44: '44px',
         42: '42px',
         40: '40px',
+        30: '30px',
         50: '50px',
         64: '64px',
+        20: '20px',
         21: '21px',
-        22: '22px'
+        22: '22px',
+        10: '10px',
+        8: '8px'
       },
       lineHeight: {
         56: '56px',
+        96: '96px',
       },
       borderRadius: {
         10: '10px',
+        20: '20px',
         22: '22px'
       },
+      maxWidth: {
+        840: '840px',
+        400: '400px',
+        360: '360px',
+      },
+      gap: {
+        40: '160px'
+      }
     },
   },
   plugins: [],
